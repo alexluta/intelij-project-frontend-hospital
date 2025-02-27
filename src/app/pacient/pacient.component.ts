8922acd0f43a65f956a5d90b0934e7d56ee201bd
alexandru_luta <alexandru.luta@kontron.ro>
2025-02-27 17:36:32 +0200

    commit inceput

--- src/app/pacient/pacient.component.ts
+++ src/app/pacient/pacient.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pacient',
-  imports: [],
   templateUrl: './pacient.component.html',
-  styleUrl: './pacient.component.css'
+  styleUrls: ['./pacient.component.css']
 })
-export class PacientComponent {
+export class PacientComponent implements OnInit {
+  pacienti: any[] = [];
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  getRaportPacienti(): void {
+    fetch('http://localhost:8080/raportPacienti')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Eroare la încărcarea pacientilor');
+        }
+        return response.json();
+      })
+      .then(pacienti => {
+        this.pacienti = pacienti;
+        alert(pacienti);
+      })
+      .catch(error => {
+        console.error('Eroare:', error);
+        alert('A apărut o problemă la încărcarea pacientilor.');
+      });
+
+  }
 }
